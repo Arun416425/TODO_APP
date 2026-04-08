c282c6e1c515e7f3860a862f075e53173ee1cdd9
Arun Ayyori <arunayyori123@gmail.com>
2026-04-08 15:34:14 +0530

    Todo_App using Angulat JS

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,42 @@
-import { Component, signal } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('todo_app');
+  newTodo = signal('');
+
+  todos = signal<{ text: string, done: boolean }[]>(JSON.parse(localStorage.getItem('todos') || "[]"));
+  
+  constructor() {
+    effect(() => {
+      localStorage.setItem('todos', JSON.stringify(this.todos()));
+    })
+  }
+
+  addTodo() {
+    const text = this.newTodo().trim()
+
+    if (text === "") {
+      alert("Please Write a todo")
+      return
+    }
+
+    this.todos.update(list => [...list, { text, done: false }])
+
+    this.newTodo.set('')
+  }
+
+  toggleTodo(index: number) {
+    this.todos.update(list => list.map((item, i) => i === index ? { ...item, done: !item.done } : item))
+  }
+
+  deleteTodo(index: number) {
+    this.todos.update(list => list.filter((_, i) => i !== index))
+  }
 }
